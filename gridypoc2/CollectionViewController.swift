241a03e3a003fe336ff78984ebc115cebc957cf6
amoswk <36827671+amoswk@users.noreply.github.com>
2020-12-20 13:57:08 -0500

    function for gameOutlet collection view

--- gridypoc2/CollectionViewController.swift
+++ gridypoc2/CollectionViewController.swift
@@ -12,7 +12,9 @@ import UIKit
 class ArrayDisplay: UIViewController, UICollectionViewDataSource {
     
     var images: [UIImage] = []
+    lazy var gameImages: [UIImage] = images.shuffled()
     
+ 
     @IBOutlet weak var collectionOutlet: UICollectionView!
     
     @IBOutlet weak var gameOutlet: UICollectionView!
@@ -23,20 +25,56 @@ class ArrayDisplay: UIViewController, UICollectionViewDataSource {
         
         collectionOutlet.collectionViewLayout = GridyLayout()
         
+        gameOutlet.collectionViewLayout = GridyLayout()
+        
         gameOutlet.dataSource = self
         
         print(images)
+        print(gameImages)
         
     }
     
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == collectionOutlet {
             return images.count
         }
-return 0
+        return 0
     }
-// 12/13 create another array (gameImages) make this array equal to images.shuffled. Modify func to display game images count for gameOutlet, modify line 42 collectionView func.
+    // 12/13 create another array (gameImages) make this array equal to images.shuffled. Modify func to display game images count for gameOutlet, modify line 42 collectionView func.
+    
+    func gameView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       
+        if gameView == gameOutlet {
+            return gameImages.count
+        }
+        return 0
+    }
+    
+    func gameView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath)
+           
+           let photo = images[indexPath.row]
+           
+           let imageView = UIImageView(frame: cell.bounds)
+           
+           imageView.image = photo
+           
+           cell.contentView.addSubview(imageView)
+           
+           
+           
+           
+           return cell
+           
+           
+
+       
+           
+       }
+    
+    
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -46,13 +84,13 @@ return 0
         let photo = images[indexPath.row]
         
         let imageView = UIImageView(frame: cell.bounds)
- 
+        
         imageView.image = photo
         
         cell.contentView.addSubview(imageView)
-    
         
-      
+        
+        
         
         return cell
         
@@ -60,9 +98,11 @@ return 0
         
         
         
-        
+    
         
     }
+    
+
     
     
     
